4d5ccc33f2d0e6d0a9915ad9108f21df424c9616
Shaziyehtajik <149049835+Shaziyehtajik@users.noreply.github.com>
2024-01-15 14:42:46 +0000

    Update MovieView.jsx

--- src/components/movie-view/MovieView.jsx
+++ src/components/movie-view/MovieView.jsx
@@ -7,16 +7,21 @@ class MovieView extends React.Component {
     return (
       <div className="movie-view">
         <div className="movie-poster">
-          <img src={movie.ImagePath} alt={`${movie.Title} Poster`} />
+          <img src={movie.thumb} alt={`${movie.title} Poster`} />
         </div>
         <div className="movie-title">
           <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
+          <span className="value">{movie.title}</span>
         </div>
         <div className="movie-description">
           <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
+          <span className="value">{movie.description}</span>
         </div>
+        <div className="movie-category">
+          <span className="label">Category: </span>
+          <span className="value">{movie.category}</span>
+        </div>
+        {/* Add more fields as needed, e.g., uuid, server, etc. */}
         <button onClick={() => onBackClick(null)}>Back</button>
       </div>
     );
